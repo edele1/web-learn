2969baf6363a21648c80f7e6c20faea67e6e90c2
edele <1377107757@qq.com>
2021-05-29 22:03:14 +0800

    exciting

---
+++ ex/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <hi />
+</template>
+
+<script>
+import hi from './components/hi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    hi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ex/src/components/hi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      hi
+      <p>{{ message }}</p>
+      <input type="button" @click="say_hi('jim')" value="..."/>
+      <ul>
+        <li v-for="item in items" :key="item.message">
+          {{ item.message }}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        
+       items: [
+          { message:'a' },
+          { message:'b' },
+          { message:'c' }
+        ]
+        return {
+          message: 'nihao'
+        }
+    },
+    methods: {
+        say_hi:function(name){
+          alert('hi,'+name)
+        },
+    }
+}
+
+</script>
+
+<style>
+
+</style>
