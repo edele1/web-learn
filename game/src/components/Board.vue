f25bc0854f159ed051bef1ee45d0e1da3764ec8c
edele <1377107757@qq.com>
2021-06-01 20:28:05 +0800

    尝试实现胜利者判断和历史记录

--- game/src/components/Board.vue
+++ game/src/components/Board.vue
@@ -6,22 +6,7 @@
                   <button v-for="(_,y) in 3" :key="y" class="square" @click="move(x,y)">
                       {{ squares[x][y] }}
                   </button>
-              </div>
-              <!-- <div class="board-row" >
-                  <button class="square"></button>
-                  <button class="square"></button>
-                  <button class="square"></button>
-              </div>
-              <div class="board-row">
-                  <button class="square"></button>
-                  <button class="square"></button>
-                  <button class="square"></button>
-              </div>
-              <div class="board-row">
-                  <button class="square"></button>
-                  <button class="square"></button>
-                  <button class="square"></button>
-              </div>  -->
+              </div>   
           </div>
       </div>
       <div class="game-info">
@@ -29,7 +14,7 @@
           <div v-else>Next player ： {{ player }}</div>
           <ol>
               <li>
-                  <button>{{ history }}</button>
+                  <button v-on:click="history">Move to:{{ step }}</button>
               </li>
           </ol>
       </div>
@@ -37,17 +22,76 @@
 </template>
 
 <script>
-const calculateWinner =squares =>{
-    consts lines =[
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
+export default {
+  name:'Board',
+data(){
+  return{
+    step:0,
+    squares:[
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ],
+    history:[
+      {x:0,y:0,content:'x'},
+      {x:1,y:1,content:'o'}
+    ],
+    player:'x',
+    winner:'',
+  }
+},
+created(){},
+mounted(){},
+function calculateWinner(squares) {
+// const calculateWinner = squares =>{
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+methods:{
+  move(x,y){
+    if(this.squares[x][y]){
+      alert('不能下在此处')
+      return
+    }
+    this.step +=1
+    this.history.push({ x,y,content:this.player})
+    this.squares[x][y]=this.player
+    this.player=this.player === 'x'?'o':'x'
+    if (calculateWinner(squares)) {
+  alert('胜负已定！');
+  return;
+}
+
+const winner = calculateWinner(squares);
+if (winner) {
+  this.status = '获胜者: ' + winner;
+  return;
+}
+
+  },
+
+},
+watch:{
+  move(x,y){
+    this.history
+  }
+  }
 }
 </script>
 
@@ -60,13 +104,6 @@ body {
 ol, ul {
   padding-left: 30px;
 }
-
-.board-row:after {
-  clear: both;
-  content: "o";
-  display: table;
-}
-
 .status {
   margin-bottom: 10px;
 }
