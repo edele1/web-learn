bc6aceeda8747236a8892b610e06450c7b8f2150
edele220 <1377107757@qq.com>
2021-07-19 22:54:29 +0800

    shopping

--- shopping-cart/src/components/ProductList.vue
+++ shopping-cart/src/components/ProductList.vue
@@ -2,24 +2,28 @@
   <div>
       <h1>Product List</h1>
       <ul>
-          <li v-for="product in products" :key="product.id">{{product.title}}-{{product.price}}</li>
+          <li v-for="product in products" :key="product.id">
+              {{product.title}}-{{product.price}}-{{product.inventory}}
+              <button @click="addProductToCart(product)">Add to Cart</button>
+            </li>
       </ul>
   </div>
 </template>
 
 <script>
-import shop from '@/api/shop'
-import store from '@/store/index'
 export default {
     computed:{
         products(){
-            return store.state.products
+            return this.$store.getters.availableProducts
+        }
+    },
+    methods:{
+        addProductToCart(product){
+            this.$store.dispatch('addProductToCart',product)
         }
     },
     created() {
-        shop.getProducts(products => {
-            store.commit('setProducts',products)
-        })
+        this.$store.dispatch('fetchProducts')
     }
 
 }
--- shopping-cart/src/store/index.js
+++ shopping-cart/src/store/index.js
@@ -1,28 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { //=data
-    products:[]
+    products:[],
+    cart:[]
   },
-  getters:{ //computed properties
-    productsCount(){
 
+  getters:{ //computed properties
+    availableProducts(state,getters){
+      return state.products.filter(product => product.inventory>0)
+    }
+
+    },
+  
+  actions:{
+    fetchProducts({commit}){
+      return new Promise((resolve,reject)=>{
+        shop.getProducts(products => {
+        commit('setProducts',products)
+        resolve()
+    })})//make the call
+      
+    },
+    addProductToCart(context,product){
+      if(product.inventory > 0){
+        const cartItem = context.state.cart.find(item => item.id === product.id)
+        if(!cartItem){
+          context.commit('pushProductToCart', product.id)
+        } else {
+          context.commit('incrementItemQuantity', cartItem)
+        }
+        context.commit('decreamentProductInventory',product)
+      }
     }
   },
-  mutations: {
+
+  mutations:{
     setProducts(state,products){
       //update products
       state.products = products
-    }
-  },
-  actions: {
-    fetchProducts(){//make the call
+    },
+    pushProductToCart(state,productId){
+      state.cart.push({
+        id:productId,
+        quantity:1
+      })
+    },
+    incrementItemQuantity(state,cartItem){
+      cartItem.quantity ++;
+    },
+    decreamentProductInventory(state,product){
+      product.inventory --;
+    },
 
-    }
+
   },
-  modules: {
+  modules:{
+
   }
+  
 })
