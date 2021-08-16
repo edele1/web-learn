ac354b4b392f4e4d3c4839ea2ab5c81740a939dd
edele220 <1377107757@qq.com>
2021-08-16 13:01:37 +0800

    知乎日报小程序

---
+++ wx/pages/detail/detail.js
@@ -0,0 +1,81 @@
+// pages/detail/detail.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        art:{},
+        content:"",
+        id:'',
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that=this
+        wx.request({
+          url: 'https://news-at.zhihu.com/api/3/news/'+options.id,
+          success:function(res){
+            // console.log(res)
+            that.setData({
+                'art':res.data,
+                'content':res.data.body
+            })
+        }
+        })
+        
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
